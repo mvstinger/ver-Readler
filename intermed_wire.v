49001b2218479219197436003dc0d98a8dbba1dc
Michael Stinger <mvstinger@gmail.com>
2018-04-28 21:29:20 -0400

    Initial commit of intermed_wire
    Initial commit of working intermed_wire and testbench.

---
+++ intermed_wire.v
@@ -0,0 +1,22 @@
+module intermed_wire (
+	in_1,
+	in_2,
+	in_3,
+	out_1,
+	out_2
+	);
+	
+	input in_1;
+	input in_2;
+	input in_3;
+	output out_1;
+	output out_2;
+	
+	wire intermediate_wire;
+	
+	assign intermediate_wire = in_1 & in_2;
+	
+	assign out_1 = intermediate_wire & in_3;
+	assign out_2 = intermediate_wire | in_3;
+	
+	endmodule;
